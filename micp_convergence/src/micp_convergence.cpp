24a6ca338cf91e45214d7c05f24d3654b1598dcf
Alexander Mock <amock@uos.de>
2023-09-10 15:22:31 +0200

    fix

--- micp_convergence/src/micp_convergence.cpp
+++ micp_convergence/src/micp_convergence.cpp
@@ -607,9 +607,9 @@ int main(int argc, char** argv)
             std::cout << "| " 
                 << std::setfill(' ') << std::setw(6) << sample_radius << " | " 
                 << std::setfill(' ') << std::setw(11) << p2l_rate * 100.0 << "% | " 
-                << std::setfill(' ') << std::setw(11) << p2l_pose_error * 100.0 << "cm | " 
+                << std::setfill(' ') << std::setw(10) << p2l_pose_error * 100.0 << "cm | " 
                 << std::setfill(' ') << std::setw(11) << spc_rate * 100.0 << "% | "
-                << std::setfill(' ') << std::setw(11) << spc_pose_error * 100.0 << "cm | ";
+                << std::setfill(' ') << std::setw(10) << spc_pose_error * 100.0 << "cm | ";
 
 
             {
